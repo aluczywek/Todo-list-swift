907281c7b500c596a2b3f4daea229452d1f2d898
aluczywek <a.luczywek@outlook.com>
2023-04-15 00:21:23 +0200

    taskCreatorVC v2

--- TodoApp/TodoApp/Controllers/TaskCreatorVC.swift
+++ TodoApp/TodoApp/Controllers/TaskCreatorVC.swift
@@ -6,18 +6,54 @@
 //
 
 import UIKit
+import CoreData
 
 class TaskCreatorVC: UITableViewController {
+    
+    var category = ""
+    weak var delegate: ViewController?
 
     @IBOutlet weak var taskNoteTextField: UITextField!
-    @IBOutlet weak var categoryPicker: UIPickerView!
     @IBOutlet weak var datePicker: UIDatePicker!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
     }
     
-    @IBAction func saveButton(_ sender: UIButton) {
+    
+    //MARK: - Segmented Control
+    
+    @IBAction func categotySegmentedControl(_ sender: UISegmentedControl) {
+        if sender.selectedSegmentIndex == 0 {
+            category = "Home"
+        }
+        else if sender.selectedSegmentIndex == 1 {
+            category = "Work"
+            
+        } else {
+            category = "Other"
+        }
     }
+    
+    
+    //MARK: - Saving new task
+    
+    @IBAction func saveButton(_ sender: UIButton) {
+        
+    }
+    
+    //MARK: - Create new task
+    
+    func createNewTask() -> TaskModel {
+        var task: TaskModel = TaskModel(
+            name: taskNoteTextField.text ?? "",
+            date: datePicker.date,
+            category: category)
+        return task
+    }
+    
+    @IBAction func cancelButton(_ sender: UIButton) {
+        self.navigationController?.popToRootViewController(animated: true)
+    }
+    
 }
--- TodoApp/TodoApp/Controllers/ViewController.swift
+++ TodoApp/TodoApp/Controllers/ViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 import CoreData
 
 class ViewController: UITableViewController {
-    let tasks = ["casdca", "hjvds", "hjgavchdv"]
+    let tasks: [TaskModel] = []
     let cellIdentifier = "TaskCell"
     
 
@@ -27,25 +27,29 @@ class ViewController: UITableViewController {
         } else {
             return 1
         }
-    
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as! TaskCell
         
-//        cell.setName(text: tasks[indexPath.row])
+        if tasks.count != 0 {
+            cell.setName(text: tasks[indexPath.row].name)
+        } else {
+            cell.setName(text: "No tasks")
+        }
+       
         return cell
     }
     
     //MARK: - TableView Delegate Methods
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
     }
 
     //MARK: - Add new task
     
     @IBAction func AddButton(_ sender: UIBarButtonItem) {
+        performSegue(withIdentifier: "goToTaskCreator", sender: self)
     }
 }
 
