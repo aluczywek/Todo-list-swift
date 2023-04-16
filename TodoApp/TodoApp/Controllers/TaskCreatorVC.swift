bbcdc7507568191d7df559612a8480d6af3029bc
aluczywek <a.luczywek@outlook.com>
2023-04-16 21:57:08 +0200

    saving to core data

--- TodoApp/TodoApp/Controllers/TaskCreatorVC.swift
+++ TodoApp/TodoApp/Controllers/TaskCreatorVC.swift
@@ -11,7 +11,7 @@ import CoreData
 class TaskCreatorVC: UITableViewController {
     
     var category = ""
-    weak var delegate: ViewController?
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
 
     @IBOutlet weak var taskNoteTextField: UITextField!
     @IBOutlet weak var datePicker: UIDatePicker!
@@ -19,7 +19,6 @@ class TaskCreatorVC: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-    
     
     //MARK: - Segmented Control
     
@@ -35,22 +34,41 @@ class TaskCreatorVC: UITableViewController {
         }
     }
     
-    
     //MARK: - Saving new task
     
     @IBAction func saveButton(_ sender: UIButton) {
-        
+        createNewTask()
+        saveNewTask()
+    }
+    
+    fileprivate func showSuccesAlert() {
+        let dialogMessage = UIAlertController(title: "Succes!", message: "New task created", preferredStyle: .alert)
+        let ok = UIAlertAction(title: "OK", style: .default, handler: { (action) -> Void in
+            self.navigationController?.popToRootViewController(animated: true)
+        })
+        dialogMessage.addAction(ok)
+        self.present(dialogMessage, animated: true)
+    }
+    
+    func saveNewTask() {
+        do {
+            try context.save()
+            showSuccesAlert()
+        } catch {
+            print("Error saving context \(error)")
+        }
     }
     
     //MARK: - Create new task
     
-    func createNewTask() -> TaskModel {
-        var task: TaskModel = TaskModel(
-            name: taskNoteTextField.text ?? "",
-            date: datePicker.date,
-            category: category)
-        return task
+    func createNewTask() {
+        let newTask = TodoTask(context: self.context)
+        newTask.name = taskNoteTextField.text ?? ""
+        newTask.date = datePicker.date
+        newTask.category = category
     }
+   
+    // MARK: - Cancel Button
     
     @IBAction func cancelButton(_ sender: UIButton) {
         self.navigationController?.popToRootViewController(animated: true)
--- TodoApp/TodoApp/Controllers/ViewController.swift
+++ TodoApp/TodoApp/Controllers/ViewController.swift
@@ -9,14 +9,21 @@ import UIKit
 import CoreData
 
 class ViewController: UITableViewController {
-    let tasks: [TaskModel] = []
+    var tasks = [TodoTask]()
     let cellIdentifier = "TaskCell"
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Todo List"
         tableView.register(UINib(nibName: cellIdentifier, bundle: nil), forCellReuseIdentifier: cellIdentifier)
+        loadTasks()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        loadTasks()
+        tableView.reloadData()
     }
     
     //MARK: - TableView DataSource Methods
@@ -33,11 +40,12 @@ class ViewController: UITableViewController {
         let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as! TaskCell
         
         if tasks.count != 0 {
-            cell.setName(text: tasks[indexPath.row].name)
+            cell.setName(text: tasks[indexPath.row].name!)
+            cell.setCategory(category: tasks[indexPath.row].category!)
+            cell.setDate(date: tasks[indexPath.row].date!)
         } else {
             cell.setName(text: "No tasks")
         }
-       
         return cell
     }
     
@@ -46,10 +54,42 @@ class ViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
     }
 
+    override func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+        return.delete
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            tableView.beginUpdates()
+            context.delete(tasks[indexPath.row])
+            tasks.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+            tableView.endUpdates()
+        }
+        do {
+            try context.save()
+        } catch {
+            print("Error saving context \(error)")
+        }
+    }
+    
+    
     //MARK: - Add new task
     
     @IBAction func AddButton(_ sender: UIBarButtonItem) {
         performSegue(withIdentifier: "goToTaskCreator", sender: self)
+    }
+    
+    //MARK: - Data manipulation methods
+    
+    func loadTasks() {
+        let request: NSFetchRequest<TodoTask> = TodoTask.fetchRequest()
+        do {
+            tasks = try context.fetch(request)
+        } catch {
+            print("Error fetching data from context.")
+        }
+        tableView.reloadData()
     }
 }
 
